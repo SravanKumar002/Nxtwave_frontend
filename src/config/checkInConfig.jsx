00385a7fe15564b633d000978f7075e38b040630
sravan <sravankumarega002@gmail.com>
2025-06-28 14:42:54 +0530

    second1

--- src/config/checkInConfig.jsx
+++ src/config/checkInConfig.jsx
@@ -8,7 +8,7 @@ export const CHECK_IN_CONFIG = {
 
   dayWindows: {
     5: { startHour: 9, startMinute: 0, endHour: 9, endMinute: 30 }, // Friday
-    6: { startHour: 10, startMinute: 0, endHour: 12, endMinute: 40 }, // Saturday
+    6: { startHour: 10, startMinute: 0, endHour: 18, endMinute: 40 }, // Saturday
   },
 
   blackoutDates: ["2023-12-25", "2024-01-01"],
